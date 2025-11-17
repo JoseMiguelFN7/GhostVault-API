82f4113119101c36767384baddc7e2191dd448da
José <os_ferreira@Legion5.localdomain>
2025-11-17 09:51:18 -0400

    new secret model, controller and creation endpoint and logic

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\SecretController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    
+    //Endpoint para crear el secreto
+    Route::post('secrets', [SecretController::class, 'store']);
+});
